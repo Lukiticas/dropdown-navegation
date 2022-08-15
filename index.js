65d3ee2af6d558ba5d3cce9c0cfbadb0de052048
Lucas Matheus Nunes Pereira <Lucasmathues416@outlook.com>
2022-08-15 16:03:58 -0300

    maked some comments

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const $ = (el) => document.querySelectorAll(el);
+const dropdown = $(".dropdown");
+const nav = $(".navbar__links")[0];
+const menu = $(".menu")[0];
 
-$(".submenu").forEach((links) => {
+// toggle --open for the arrow up transformation
+// and "open" for the dropdown
+dropdown.forEach((links) => {
   links.addEventListener("click", (e) => {
     e.stopPropagation();
     links.children[0].children[0].classList.toggle("--open");
@@ -9,9 +14,8 @@ $(".submenu").forEach((links) => {
   });
 });
 
-$(".menu")[0].addEventListener("click", () => {
-  const nav = $(".navbar__links")[0];
-  const menu = $(".menu")[0];
+//change the hamburguer image
+menu.addEventListener("click", () => {
   nav.classList.toggle("active");
   if (nav.classList.contains("active")) {
     menu.src = "./images/icon-close-menu.svg";
@@ -20,8 +24,17 @@ $(".menu")[0].addEventListener("click", () => {
   }
 });
 
-$(".submenu__links").forEach((e) =>
+//just stop the annoying dropdown's behaviour of closing when clicked inside
+$(".dropdown__links").forEach((e) =>
   e.addEventListener("click", (e) => {
     e.stopPropagation();
   })
 );
+
+// clicking anywhere outside the dropdown area will close it
+document.addEventListener("click", (e) => {
+  dropdown.forEach((links) => {
+    links.children[0].children[0].classList.remove("--open");
+    links.children[1].classList.remove("open");
+  });
+});
